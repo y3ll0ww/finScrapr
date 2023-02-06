84aaff18894bd7bba1935bbea478c64281a70e8a
y3ll0ww <jelle.van.geel@sogeti.com>
2023-02-06 14:18:18 +0100

    Updating databse with new information tried and tested + some minor other changes

--- actions/DatabaseActions.py
+++ actions/DatabaseActions.py
@@ -16,37 +16,66 @@ def create():
             continue
 
 
+def clear():
+    select_db()
+
+    print(error("WARNING: selected database \"") + colored(YELLOW, DatabaseInsertion.selected_database) +
+          error("\" will be whiped of all data. Do you wish to proceed?"))
+
+    while True:
+        choice = input(user_input("Y/N: "))
+
+        if choice.upper() not in ["Y", "N"]:
+            print(error("Type either \"Y\" or \"N\" (not case sensitive)"))
+        elif choice.upper() == "Y":
+
+            DatabaseInsertion.clear_database()
+
+            print("Clearing of database \"" +
+                  colored(YELLOW, DatabaseInsertion.selected_database) + "\" " + colored(GREEN, "successful"))
+
+            break
+        elif choice.upper() == "N":
+            print("Clearing of database \"" +
+                  colored(YELLOW, DatabaseInsertion.selected_database) + "\" " + colored(RED, "aborted"))
+
+            break
+        else:
+            print(error("Err: Something went wrong, please try again later"))
+
+
 def insert():
+    select_db()
+    select_dir()
+
+    num_files = len(File.list_files(DatabaseInsertion.path_statements + DatabaseInsertion.selected_folder))
+    print(colored(CYAN, str(num_files)) + " files will be inserted into database \""
+          + colored(YELLOW, DatabaseInsertion.selected_database) + "\"...\n")
+
+    DatabaseInsertion.insert()
+
+
+def select_db():
     available_databases = File.list_files(DatabaseInsertion.path_databases)
 
     if len(available_databases) < 1:
         print(error("Err: There are no available databases found, please create one first."))
     else:
         print(colored(CYAN, "Select an available database:", True))
-        select_db(available_databases)
-        select_dir()
 
-        num_files = len(File.list_files(DatabaseInsertion.path_statements))
-        print(colored(CYAN, str(num_files)) + " files will be inserted into database \""
-              + colored(YELLOW, DatabaseInsertion.selected_database) + "\"...\n")
+        counter = 1
+        for database in available_databases:
+            print("#" + str(counter) + " " + colored(YELLOW, database))
+            counter += 1
 
-        DatabaseInsertion.insert()
-
-
-def select_db(available_databases):
-    counter = 1
-    for database in available_databases:
-        print("#" + str(counter) + " " + colored(YELLOW, database))
-        counter += 1
-
-    while True:
-        num = input(user_input("\nSelect database: ") + "#")
-        try:
-            DatabaseInsertion.selected_database = available_databases[int(num)-1]
-            print("Selected \"" + colored(YELLOW, DatabaseInsertion.selected_database) + "\"...\n")
-            break
-        except:
-            print(error("Err: Invalid input; type a valid number of the database in the list"))
+        while True:
+            num = input(user_input("\nSelect database: ") + "#")
+            try:
+                DatabaseInsertion.selected_database = available_databases[int(num)-1]
+                print("Selected \"" + colored(YELLOW, DatabaseInsertion.selected_database) + "\"...\n")
+                break
+            except:
+                print(error("Err: Invalid input; type a valid number of the database in the list"))
 
 
 def select_dir():
@@ -63,9 +92,8 @@ def select_dir():
     while True:
         num = input(user_input("\nSelect folder: ") + "#")
         try:
-            selected_folder = available_folders[int(num)-1]
-            DatabaseInsertion.path_statements += selected_folder + "/"
-            print("\nSelected \"" + colored(YELLOW, selected_folder) + "\"...")
+            DatabaseInsertion.selected_folder = available_folders[int(num)-1] + "/"
+            print("\nSelected \"" + colored(YELLOW, DatabaseInsertion.selected_folder.replace("/", "")) + "\"...")
             break
         except:
             print(error("Err: Invalid input; type a valid number of the folder in the list"))
